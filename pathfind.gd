9f578caf0029a2a4ac1aa12ca3d84400070a7cc2
xolatgames <ilyuha@xolatgames.ru>
2024-11-07 18:32:57 +0300

    Optimization Update

--- pathfind.gd
+++ pathfind.gd
@@ -1,22 +1,33 @@
 extends Node2D
 
-@export var map_size = Rect2i(Vector2i(0, 0), Vector2i(36, 20))
-@export var draw_debug : bool = false
+@export var map_size: Rect2i = Rect2i(Vector2i(0, 0), Vector2i(36, 20))
+@export var draw_debug: bool = false
 
 var redraw_timer = Timer.new()
+
 
 func _ready() -> void:
 	add_child(redraw_timer)
 	redraw_timer.timeout.connect(queue_redraw)
 	redraw_timer.start(0.1)
 
+
 func _draw() -> void:
 	if draw_debug:
-		var path_grid = get_node("Player").path_grid
+		var path_grid: Array[int] = get_node("Player").path_grid
+		var solid_tiles: Array[bool] = get_node("Player").solid_tiles
+		
+		var tile: int
 		
 		for x in range(map_size.position.x, map_size.size.x):
 			for y in range(map_size.position.y, map_size.size.y):
-				draw_string(ThemeDB.fallback_font, Vector2i(x * 32, (y * 32) + 32), str(path_grid[str(x)+";"+str(y)]), HORIZONTAL_ALIGNMENT_LEFT, -1, 16, Color.GRAY)
+				tile = x + map_size.size.x * y
+				
+				draw_string(ThemeDB.fallback_font, Vector2i(x * 32, (y * 32) + 32), str(path_grid[tile]), HORIZONTAL_ALIGNMENT_LEFT, -1, 16, Color.GRAY)
+				
+				if solid_tiles[tile]:
+					draw_rect(Rect2(Vector2i(x * 32, y * 32), Vector2(32, 32)), Color(Color.RED, 0.5))
+
 
 func _on_debug_drawing_toggled(toggled_on: bool) -> void:
 	draw_debug = toggled_on
--- player.gd
+++ player.gd
@@ -1,81 +1,101 @@
 extends CharacterBody2D
 
-@export var speed = 100
+@export var speed: float = 250
 
-var map_size : Rect2i
-var path_grid : Dictionary
-var solid_tiles : Dictionary
-var player_pos : Vector2i
-var add_cells : Array [Vector2i]
-var next_pos : Vector2i
-var target : Vector2i
+var map_size: Rect2i
+var path_grid: Array[int]
+var solid_tiles: Array[bool]
+var player_pos: Vector2i
+var add_cells: Array [Vector2i]
+var next_pos: Vector2i
+var target: Vector2i
 
-@onready var tileMap = get_parent().get_node("TileMapLayer")
+@onready var tileMap = get_parent().get_node("Tiles")
 var moving_timer = Timer.new()
+
 
 func _ready() -> void:
 	player_pos = global_position / 32
 	
 	map_size = get_tree().current_scene.map_size
 	
+	var tile: int
+	
 	for x in range(map_size.position.x, map_size.size.x):
 		for y in range(map_size.position.y, map_size.size.y):
-			path_grid[str(x)+";"+str(y)] = -1
+			path_grid.append(-1)
+			solid_tiles.append(false)
 	
 	target = player_pos
 	next_pos = player_pos
 	
 	for x in range(map_size.position.x, map_size.size.x):
 		for y in range(map_size.position.y, map_size.size.y):
+			tile = x + map_size.size.x * y
+			
 			if tileMap.get_cell_atlas_coords(Vector2i(x, y)) == Vector2i(0, 0):
-				solid_tiles[str(x)+";"+str(y)] = true
-			else:
-				solid_tiles[str(x)+";"+str(y)] = false
+				solid_tiles[tile] = true
+
 
 func _unhandled_input(event: InputEvent) -> void:
+	var tile: int
+	
 	if event is InputEventMouseButton:
 		if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-			if solid_tiles[str(int(event.position.x / 32))+";"+str(int(event.position.y / 32))] == false:
-				target = event.position / 32
+			tile = int(event.position.x/32) + map_size.size.x * int(event.position.y/32)
+			
+			if solid_tiles[tile] == false:
+				target = Vector2i(event.position/32)
 				_move()
+
 
 func _move() -> void:
 	player_pos = global_position / 32
 	
+	var tile: int
+	
 	for x in range(map_size.position.x, map_size.size.x):
 		for y in range(map_size.position.y, map_size.size.y):
-			path_grid[str(x)+";"+str(y)] = -1
+			tile = x + map_size.size.x * y
+			
+			path_grid[tile] = -1
 	
 	next_pos = target
 	
 	if target != player_pos:
-		if solid_tiles[str(target.x)+";"+str(target.y)] == false:
-			path_grid[str(target.x)+";"+str(target.y)] = 0
+		tile = target.x + map_size.size.x * target.y
 		
-		var calculated : bool = false
+		if solid_tiles[tile] == false:
+			path_grid[tile] = 0
+		
+		var calculated: bool = false
 		
 		while(!calculated):
 			add_cells.clear()
 			
 			for x in range(map_size.position.x, map_size.size.x):
 				for y in range(map_size.position.y, map_size.size.y):
-					if path_grid[str(x)+";"+str(y)] == 0:
+					tile = x + map_size.size.x * y
+					
+					if path_grid[tile] == 0:
 						_add_cell(x - 1, y)
 						_add_cell(x + 1, y)
 						_add_cell(x, y - 1)
 						_add_cell(x, y + 1)
 						
-						path_grid[str(x)+";"+str(y)] += 1
+						path_grid[tile] += 1
 						
 						if player_pos == Vector2i(x, y):
 							calculated = true
 					
-					elif  path_grid[str(x)+";"+str(y)] > 0:
-						path_grid[str(x)+";"+str(y)] += 1
+					elif  path_grid[tile] > 0:
+						path_grid[tile] += 1
 			
 			for a in add_cells:
-				if path_grid[str(a.x)+";"+str(a.y)] == -1:
-					path_grid[str(a.x)+";"+str(a.y)] = 0
+				tile = a.x + map_size.size.x * a.y
+				
+				if path_grid[tile] == -1:
+					path_grid[tile] = 0
 			
 			if add_cells.size() < 2:
 				target = player_pos
@@ -89,6 +109,7 @@ func _move() -> void:
 	if target == player_pos:
 		next_pos = player_pos
 
+
 func _physics_process(delta: float) -> void:
 	velocity = global_position.direction_to(next_pos * 32) * speed
 	move_and_slide()
@@ -97,16 +118,22 @@ func _physics_process(delta: float) -> void:
 		global_position = next_pos * 32
 		_move()
 
-func _add_cell(x, y):
+
+func _add_cell(x, y) -> void:
+	var tile: int = x + map_size.size.x * y
+	
 	if x < map_size.position.x or y < map_size.position.y or x > map_size.size.x - 1 or y > map_size.size.y - 1:
 		return
 	
-	if solid_tiles[str(x)+";"+str(y)] == false:
+	if solid_tiles[tile] == false:
 		add_cells.append(Vector2i(x, y))
 
-func _get_next_pos(x, y):
+
+func _get_next_pos(x, y) -> void:
+	var tile: int = (player_pos.x + x) + map_size.size.x * (player_pos.y + y)
+	
 	if player_pos.x + x < map_size.position.x or player_pos.y + y < map_size.position.y or player_pos.x + x > map_size.size.x - 1 or player_pos.y + y > map_size.size.y - 1:
 		return
 	
-	if path_grid[str(player_pos.x + x)+";"+str(player_pos.y + y)] == 2:
+	if path_grid[tile] == 2:
 		next_pos = Vector2i(player_pos.x + x, player_pos.y + y)
